81a0b25254a7602083c075e950ada695495a5fad
Barry Lam <barrylam@Barrys-MacBook-Pro.local>
2017-12-16 23:33:20 -0500

    first publish

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,9 +2,10 @@
 Exercise #1
 Create a variable named "numbers" and assign an empty array to it.
 */
-var numbers;
+var numbers = [];
 
-
+// console.log('meow meow')
+// console.log('woof woof')
 /*
 Exercise #2
 Using the loop provided for you below, use the array push method to insert the numbers 1 to 10 into the array.
@@ -12,6 +13,7 @@ Using the loop provided for you below, use the array push method to insert the n
 
 for(var i=0;i<10;i++){
     //use the array push() method here.
+    numbers.push(i)
 }
 
 
@@ -19,8 +21,8 @@ for(var i=0;i<10;i++){
 Exercise #3
 Test that you used the array push method correctly by console logging the first item in the array. Console log the last item in the array afterwards.
 */
-console.log();
-console.log();
+console.log(numbers[0]);
+console.log(numbers[9]);
 
 
 /*
@@ -28,6 +30,9 @@ Exercise #5
 Create an empty object and assign it to the variable car below.
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
-var car;
+var car = {}
+
+car.colour="black";
+console.log(car.colour)
 
 
